ca6c06d14799fe99f8bd196b17a3715193f7514c
Geoffrey Huntley <ghuntley@ghuntley.com>
2022-12-15 21:05:44 +1000

    Update scrape-elonmusk.sh

--- scrape-elonmusk.sh
+++ scrape-elonmusk.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo ${TWTY_CONFIG} > ~/.config/twty/settings.json
+mkdir -p /home/runner/.config/twty
+echo ${TWTY_CONFIG} > /home/runner/.config/twty/settings.json
 export ELONID=`twty -u elonmusk --json | tail -n1 | jq .id_str | tr -d \"`
 cat template.html | sed "s/ELONID/${ELONID}/" > index.html
 
